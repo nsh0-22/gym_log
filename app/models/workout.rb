81dc43a65a52a5935a85ff6b023f74e40bf9527e
nsh0-22 <boc228sk@gmail.com>
2026-08-03 22:40:59 +0900

    Add workout validations

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,8 @@
 class Workout < ApplicationRecord
   belongs_to :exercise
+  validates :exercise_id, {presence: true}
+  validates :weight, {presence: true}
+  validates :reps, {presence: true}
+  validates :sets, {presence: true}
+  validates :date, {presence: true}
 end
