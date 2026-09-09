8ba810a88754cf24b3b6a557522a92c4eeef3193
nsh0-22 <boc228sk@gmail.com>
2026-09-09 22:17:22 +0900

    Add user signup

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,4 +1,17 @@
 class SignupsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      password: params[:password]
+    )
+
+    if user.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_07_29_113025) do
+ActiveRecord::Schema[7.0].define(version: 2026_09_09_124902) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2026_07_29_113025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_exercises_on_category_id"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "workouts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
