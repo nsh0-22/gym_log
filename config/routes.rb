4847a14ab814c399e20e632bd4cf86b4427cb80d
nsh0-22 <boc228sk@gmail.com>
2026-09-09 21:15:13 +0900

    Add user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'signups/new'
   resources :workouts
   root "workouts#index"
+  resource :signup, only: %i[ new create ] 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
